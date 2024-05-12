ca5c9053554c0a918e01e70fa29bed1e94ccf2e3
Terence Ralph <terenceralph.ragaas@gmail.com>
2024-05-12 22:31:57 +0800

    added event for x and o pattern,and some win pattern

--- games/index.js
+++ games/index.js
@@ -1,8 +1,12 @@
 const box = document.querySelectorAll('.box');
 
 const oxo = [];
+const xPattern = [];
+const oPattern = [];
 for (let index = 0; index < box.length; index++) {
     box[index].addEventListener('click', function() {
+
+
         if(oxo.length === 0){
 
             oxo.push( this.textContent = 'x');
@@ -16,31 +20,40 @@ for (let index = 0; index < box.length; index++) {
 
             }
             else if ( oxo[(oxo.length -1)] === 'o'){
-             oxo.push( this.textContent = 'x');
-             this.textContent = 'x';
-
+                oxo.push( this.textContent = 'x');
+                this.textContent = 'x';
             }
             
         }
-         
+        if(this.textContent === 'x'){
+            // x pattern 
+            xPattern.push(Number(this.classList[2]));
+            sortPattern(xPattern);
+            console.log(xPattern);
+            win(xPattern);
+            
+        } else {
+            // o pattern
+            oPattern.push(Number(this.classList[2]));
+            sortPattern(oPattern);
+            console.log(oPattern);
+            win(oPattern);
 
-        // console.log(oxo);
-        winnablePattern(oxo, 3);
+        }
     }); 
 }
 
-function winnablePattern(oxo, elementsPerSubArray) {
-    console.log(oxo);
-    var matrix = [], i, k;
-
-    for (i = 0, k = -1; i < oxo.length; i++) {
-        if (i % elementsPerSubArray === 0) {
-            k++;
-            matrix[k] = [];
-        }
-
-        matrix[k].push(oxo[i]);
-    }
-
-    return matrix;
+function win(value) {
+ 
+    const winPattern=   [   [1,2,3],[4,5,6],
+                            [7,8,9],[1,4,7],
+                            [2,5,8],[3,6,9],
+                            [1,5,9],[3,5,7] 
+                        ];
 }
+    
+function sortPattern(value) {
+    value.sort();
+
+}
+
